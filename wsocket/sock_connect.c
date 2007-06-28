1168da963ee8abf26d31c248089dc7d68ebe0e84
Luca Barbato <lu_zero@gentoo.org>
2007-06-28 20:10:45 +0200

    GPL -> LGPL

--- wsocket/sock_connect.c
+++ wsocket/sock_connect.c
@@ -1,27 +1,22 @@
 /* * 
- *  $Id$
- *  
- *  This file is part of NetEmbryo 
+ * * This file is part of NetEmbryo
  *
- * NetEmbryo -- default network wrapper 
- *
- *  Copyright (C) 2005 by
- *  	
- *	- Federico Ridolfo	<federico.ridolfo@polito.it>
+ * Copyright (C) 2007 by LScube team <team@streaming.polito.it>
+ * See AUTHORS for more details
  * 
- *  NetEmbryo is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * NetEmbryo is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- *  NetEmbryo is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * NetEmbryo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with NetEmbryo; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with NetEmbryo; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  *  
  *  this piece of code is taken from NeMeSI source code
  * */
